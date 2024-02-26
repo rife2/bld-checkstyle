fb132d7dc9a830a435825417a35fb80245e339d7
Erik C. Thauvin <erik@thauvin.net>
2024-02-26 02:24:19 -0800

    Bumped to bld 1.9.0

--- src/bld/java/rife/bld/extension/CheckstyleOperationBuild.java
+++ src/bld/java/rife/bld/extension/CheckstyleOperationBuild.java
@@ -35,7 +35,7 @@ public class CheckstyleOperationBuild extends Project {
     public CheckstyleOperationBuild() {
         pkg = "rife.bld.extension";
         name = "CheckstyleOperation";
-        version = version(0, 9, 2, "SNAPSHOT");
+        version = version(0, 9, 2);
 
         javaRelease = 17;
         downloadSources = true;
@@ -43,7 +43,7 @@ public class CheckstyleOperationBuild extends Project {
         repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
 
         scope(compile)
-                .include(dependency("com.uwyn.rife2", "bld", version(1, 8, 0)));
+                .include(dependency("com.uwyn.rife2", "bld", version(1, 9, 0)));
         scope(test)
                 .include(dependency("com.puppycrawl.tools", "checkstyle", version(10, 13, 0)))
                 .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 10, 2)))
