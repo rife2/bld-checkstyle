41169b88d92f8b0545d945a9c5c597b14427469d
Erik C. Thauvin <erik@thauvin.net>
2024-09-29 17:36:13 -0700

    Added soft assertions

--- src/test/java/rife/bld/extension/CheckstyleOperationTest.java
+++ src/test/java/rife/bld/extension/CheckstyleOperationTest.java
@@ -16,6 +16,7 @@
 
 package rife.bld.extension;
 
+import org.assertj.core.api.AutoCloseableSoftAssertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import rife.bld.BaseProject;
@@ -90,15 +91,17 @@ class CheckstyleOperationTest {
                 .treeWithJavadoc(true)
                 .executeConstructProcessCommandList();
 
-        for (var p : args) {
-            var found = false;
-            for (var a : params) {
-                if (a.startsWith(p)) {
-                    found = true;
-                    break;
+        try (var softly = new AutoCloseableSoftAssertions()) {
+            for (var p : args) {
+                var found = false;
+                for (var a : params) {
+                    if (a.startsWith(p)) {
+                        found = true;
+                        break;
+                    }
                 }
+                softly.assertThat(found).as(p + " not found.").isTrue();
             }
-            assertThat(found).as(p + " not found.").isTrue();
         }
     }
 
