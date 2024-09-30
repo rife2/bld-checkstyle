fc84ae9d60fb06361fc44ced29bbf076f5f7f60e
Erik C. Thauvin <erik@thauvin.net>
2024-09-29 17:40:53 -0700

    Version 1.0.5

--- src/bld/java/rife/bld/extension/CheckstyleOperationBuild.java
+++ src/bld/java/rife/bld/extension/CheckstyleOperationBuild.java
@@ -33,7 +33,7 @@ public class CheckstyleOperationBuild extends Project {
     public CheckstyleOperationBuild() {
         pkg = "rife.bld.extension";
         name = "CheckstyleOperation";
-        version = version(1, 0, 4);
+        version = version(1, 0, 5);
 
         javaRelease = 17;
 
