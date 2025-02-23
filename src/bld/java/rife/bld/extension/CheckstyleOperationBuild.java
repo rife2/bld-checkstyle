c6c80b6f3fabd1c0c84e9a516d0a9ade5764bbcb
Erik C. Thauvin <erik@thauvin.net>
2025-02-23 15:34:03 -0800

    Bump JUnit to version 5.12.0

--- src/bld/java/rife/bld/extension/CheckstyleOperationBuild.java
+++ src/bld/java/rife/bld/extension/CheckstyleOperationBuild.java
@@ -46,8 +46,8 @@ public class CheckstyleOperationBuild extends Project {
                 .include(dependency("com.uwyn.rife2", "bld", version(2, 2, 0)));
         scope(test)
                 .include(dependency("com.puppycrawl.tools", "checkstyle", version(10, 21, 2)))
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 11, 4)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 11, 4)))
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 12, 0)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 12, 0)))
                 .include(dependency("org.assertj", "assertj-core", version(3, 27, 3)));
 
         javadocOperation()
