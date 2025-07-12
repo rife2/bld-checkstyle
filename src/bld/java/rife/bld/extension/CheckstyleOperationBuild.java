35bfede2366969db3b5a0783bee4fcde9d3278e5
Erik C. Thauvin <erik@thauvin.net>
2025-07-11 23:06:01 -0700

    Bump bld to version 2.3.0

--- src/bld/java/rife/bld/extension/CheckstyleOperationBuild.java
+++ src/bld/java/rife/bld/extension/CheckstyleOperationBuild.java
@@ -47,7 +47,7 @@ public class CheckstyleOperationBuild extends Project {
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL, RIFE2_RELEASES, RIFE2_SNAPSHOTS);
 
         scope(compile)
-                .include(dependency("com.uwyn.rife2", "bld", version(2, 2, 1)));
+                .include(dependency("com.uwyn.rife2", "bld", version(2, 3, 0)));
         scope(test)
                 .include(dependency("com.puppycrawl.tools", "checkstyle", version(10, 26, 1)))
                 .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 13, 2)))
