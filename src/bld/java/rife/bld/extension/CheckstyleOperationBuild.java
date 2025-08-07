18ead26c3d6c2f184ddb3beeb3e487f6eb260ae5
Erik C. Thauvin <erik@thauvin.net>
2025-08-07 00:30:32 -0700

    Bump Checkstyle to version 11.0.0

--- src/bld/java/rife/bld/extension/CheckstyleOperationBuild.java
+++ src/bld/java/rife/bld/extension/CheckstyleOperationBuild.java
@@ -49,7 +49,7 @@ public class CheckstyleOperationBuild extends Project {
         scope(compile)
                 .include(dependency("com.uwyn.rife2", "bld", version(2, 3, 0)));
         scope(test)
-                .include(dependency("com.puppycrawl.tools", "checkstyle", version(10, 26, 1)))
+                .include(dependency("com.puppycrawl.tools", "checkstyle", version(11, 0, 0)))
                 .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 13, 4)))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 13, 4)))
                 .include(dependency("org.assertj", "assertj-core", version(3, 27, 3)));
