7d82d4f912466ad0aad34cd188ff973cc2c3fbb7
Erik C. Thauvin <erik@thauvin.net>
2025-10-26 11:22:26 -0700

    Bump Checkstyle to version 12.1.1

--- examples/src/bld/java/com/example/ExamplesBuild.java
+++ examples/src/bld/java/com/example/ExamplesBuild.java
@@ -26,7 +26,7 @@ public class ExamplesBuild extends BaseProject {
 
         repositories = List.of(MAVEN_CENTRAL);
 
-        scope(test).include(dependency("com.puppycrawl.tools", "checkstyle", version(12, 1, 0)));
+        scope(test).include(dependency("com.puppycrawl.tools", "checkstyle", version(12, 1, 1)));
 
         testOperation().mainClass("com.example.ExamplesTest");
     }
